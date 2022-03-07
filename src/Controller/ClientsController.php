a4a178aad098c135e16056d9e66178cf5f6e36e7
NayLocks <jonathan@delannoy.tech>
2022-03-07 18:00:36 +0100

    add clients part 1

--- src/Controller/ClientsController.php
+++ src/Controller/ClientsController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Customers;
+use App\Entity\CustomersAddress;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ClientsController extends AbstractController
 {
     /**
-     * @Route("/sf", name="clients")
+     * @Route("/Tiers/Clients/Clients_Liste", name="clients")
      */
-    public function index()
+    public function clients(Request $request)
     {
-        return $this->render('clients.html.twig');
+        $allClients = $this->getDoctrine()->getRepository(Customers::class);
+        $clients = $allClients->findAll();
+
+        return $this->render('Tiers/Clients/clients.html.twig', ['clients' => $clients]);
+    }
+
+    /**
+     * @Route("/Tiers/Clients/Client/{customerCode}", name="client_details")
+     */
+    public function client_details(Request $request, $customerCode)
+    {
+        $allClients = $this->getDoctrine()->getRepository(Customers::class);
+        $client = $allClients->findOneBy(array('customerCode' => $customerCode ));
+
+        return $this->render('Tiers/Clients/client.html.twig', ['client' => $client]);
     }
 }
 
